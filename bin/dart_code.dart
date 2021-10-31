32b4e9ccf93175d045bf7308fa69ba97f1b33dd7
Syafiqah <izzansuga@gmail.com>
2021-10-31 22:14:20 +0800

    update latest

--- bin/dart_code.dart
+++ bin/dart_code.dart
@@ -1,12 +1,39 @@
-// ignore_for_file: unnecessary_brace_in_string_interps
+// ignore_for_file: unnecessary_brace_in_string_interps, prefer_typing_uninitialized_variables
 
 import 'dart:convert';
 import 'dart:async';
-// import 'dart:io';
+import 'dart:io';
 import 'package:web_socket_channel/io.dart';
-//import 'package:web_socket_channel/status.dart';
 
-void main(List<String> arguments){
+
+void main(List<String> arguments) {
+
+  final channel = IOWebSocketChannel.connect(
+      'wss://ws.binaryws.com/websockets/v3?app_id=1089');
+
+  channel.stream.listen((activeSymbol) {
+    final decodeList = jsonDecode(activeSymbol);
+    final nameSymbol = decodeList['active_symbols'];
+
+
+    dynamic symbol;
+
+    for (var i =0;i<78;i++){
+      symbol = nameSymbol[i]['symbol'];
+      print("List of symbol: ${symbol}");
+    }
+    
+
+    listActive();
+    channel.sink.close();
+  });
+  
+channel.sink.add('{"active_symbols": "brief"}');
+
+}
+
+void listActive(){
+
   final channel = IOWebSocketChannel.connect(
       'wss://ws.binaryws.com/websockets/v3?app_id=1089');
 
@@ -17,13 +44,16 @@ void main(List<String> arguments){
     final time = decodeSymbol['tick']['epoch'];
     final serverTime = DateTime.fromMillisecondsSinceEpoch(time * 1000);
 
-    
     print("Name: ${name}, Price: ${price}, TimeDate: ${serverTime}");
-  
 
     //closing channel after 30 seconds
-    await Future.delayed(Duration(seconds: 30), () { print(" Closing ${name}"); });
+    await Future.delayed(Duration(seconds: 30), () {
+      print(" Closing ${name}");
+    });
     channel.sink.close();
   });
-  channel.sink.add('{"ticks": "R_50", "subscribe": 1}');
+   print('Please Enter Symbol Name : ');
+  final userInput = stdin.readLineSync();
+  channel.sink.add('{"ticks":"$userInput"}');
+
 }
